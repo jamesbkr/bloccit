5a80c91d94337bdd61587bbdd4b105a74bb91a38
James Becker <becker.james90@gmail.com>
2015-09-14 20:42:22 +0000

    fix initialize file for mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
-if Rails.env.development?
-    ActionMailer::Base.delivery_method = :smtp
+
+    ActionMailer::Base.delivery_method = 'smtp'
     ActionMailer::Base.smtp_settings = {
         address: 'smtp,sendgrid.net',
         port: '25',
@@ -9,4 +9,4 @@ if Rails.env.development?
         domain:         'heroku.com',
         enable_starttls_auto: true
     }
-  end
+ 
