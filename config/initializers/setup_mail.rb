b0da4fac21a9ad5b48adf374f37813d8c2d7a78b
James Becker <becker.james90@gmail.com>
2015-09-15 22:00:05 +0000

    mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 
-    ActionMailer::Base.delivery_method = 'smtp'
+    ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
         address: 'smtp.sendgrid.net',
         port: '587',
