5a9e7208710adf1e5fe148498e9c6e4f85e612f4
James Becker <becker.james90@gmail.com>
2015-09-16 22:07:04 +0000

    add up vote to new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
     default_scope { order('rank DESC') }
     
     scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
-    
+    after_create :create_vote
     
     validates :title, length: { minimum: 5}, presence: true
     validates :body, length: { minimum:20}, presence: true
@@ -36,5 +36,12 @@ class Post < ActiveRecord::Base
      update_attribute(:rank, new_rank)
    end
     
+    
+   private 
+    def create_vote
+      user.votes.create(value: 1, post: self)
+      
+      
+     end
     
 end
